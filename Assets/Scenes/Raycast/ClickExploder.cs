fed8605d5b05556b1b81cbc34e3cb78a5f4f66c7
Husinyuszi <husinyuszi13@gmail.com>
2023-05-25 21:12:15 +0200

    0525

--- Assets/Scenes/Raycast/ClickExploder.cs
+++ Assets/Scenes/Raycast/ClickExploder.cs
@@ -2,10 +2,69 @@ using UnityEngine;
 
  class ClickExploder : MonoBehaviour
 {
+    [SerializeField] Transform cursorTransform;
+    [SerializeField] LayerMask clickMask;
+
+    [SerializeField] float range = 10;
+    [SerializeField] float maxImpulse = 10;
+
+    ParticleSystem particleSys;
+    AudioSource audioSource;
+    Renderer cursorRenderer;
+     void Start()
+    {
+        particleSys= cursorTransform.GetComponent<ParticleSystem>();
+        audioSource = cursorTransform.GetComponent<AudioSource>();
+        cursorRenderer = cursorTransform.GetComponent<Renderer>();
+    }
+
     void Update()
     {
-        Vector3 mousePos=Input.mousePosition;
+        Vector3 mousePos = Input.mousePosition;
+        Ray ray = Camera.main.ScreenPointToRay(mousePos);
 
-        Ray ray= Camera.main.ScreenPointToRay( mousePos ); //ray sugar és 2 vector
+        bool isHit = Physics.Raycast(ray, out RaycastHit hit , float.MaxValue, clickMask) ;
+
+       // cursorTransform.gameObject.SetActive(isHit);
+       cursorRenderer.enabled = isHit;
+
+        if (isHit)
+        {
+            // Debug.Log($"Hit:  {hit.collider.name}   -   {hit.point}");
+            cursorTransform.position = hit.point;
+            if(Input.GetMouseButtonDown(0)) 
+            
+            {
+                Explode(hit.point);
+            }
+        }
+    }
+
+    void Explode (Vector3 point)
+    {
+        audioSource.Play();
+        particleSys.Play();
+
+        Rigidbody[] allRigidBodies=FindObjectsOfType<Rigidbody>();
+
+        foreach (Rigidbody rb in allRigidBodies)
+        {
+            Vector3 distanceVector = rb.position - point;
+            float distance =distanceVector.magnitude; 
+            if (distance > range)
+                continue;
+
+            //Ellökés:
+
+            Vector3 direction = distanceVector / distance;
+            float m =1-( distance / range);
+            float impulse = maxImpulse * m;
+
+            rb.AddForce(direction * impulse, ForceMode.Impulse); // ugyan az a két sor 
+            //rb.velocity += direction * impulse / rb.mass;
+           
+
+        }
+
     }
 }
